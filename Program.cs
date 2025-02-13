1949eb5e0df8521e0d14087e5f1c25e51ac9b6e5
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2025-02-13 23:51:14 +0500

    Add Comment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This project adds two numbers together and prints the result to the console.
+string firstInput = Console.ReadLine();
+string secondInput = Console.ReadLine();
+
+int firstNumber = Convert.ToInt32(firstInput);
+int secondNumber = Convert.ToInt32(secondInput);
+
+Console.WriteLine(firstNumber + secondNumber);
